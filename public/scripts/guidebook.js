fbfbe60ffddbf9b75ea832f5cd8cb5424bb574ac
Dawn Eggleton <dawn.eggleton@carleton.ca>
2019-02-04 22:27:21 -0500

    modal pop ups added, laid out, and made responsive

--- public/scripts/guidebook.js
+++ public/scripts/guidebook.js
@@ -26,5 +26,42 @@ $(document).ready( function() {
         $('.nav.species').hide();
         $('.nav.size').hide();
         $('.nav.color').show();
-	});
+    });
+
+    // jQuery Modal Popups Code by Black of Code
+    // http://code.jfbs.net/?showtopic=57
+    var fadeBackground = $(document.createElement("div")).hide().attr("id", "fade-background"),
+        modalWindow = $(document.createElement("div")).hide().attr("id", "modal-window");
+
+    $(document.body).append(fadeBackground, modalWindow);
+
+    $(document).on("click", ".modal-link", function(e) {
+        $("#fade-background").css({ "opacity": "1.0" }).fadeIn("fast");
+        var newContents = $("#" + $(this).data("modal-target")).html();
+        $("#modal-window").html(newContents);
+        $("#modal-window").fadeIn("fast");
+        e.preventDefault();
+    });
+
+    $(document).on("click", ".modal-close", function() {
+        $("#fade-background").fadeOut("fast");
+        $("#modal-window").fadeOut("fast");
+    });
+
+    $(document).on("click", ".modal-close-skills", function() {
+        $("#fade-background").fadeOut("fast");
+        $("#modal-window").fadeOut("fast");
+    });
+    $(document).on("click", "#fade-background", function() {
+        $("#fade-background").fadeOut("fast");
+        $("#modal-window").fadeOut("fast");
+    });
+
+    $(document).keypress(function(e) {
+        if(e.keyCode == 27) {
+            $("#fade-background").fadeOut("fast");
+            $("#modal-window").fadeOut("fast");
+        }
+    });
+
 });
